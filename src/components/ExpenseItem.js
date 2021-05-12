3163ed0744f46f563e1911ecb89613ee41a786db
Helena <helenasleterkesto@gmail.com>
2021-05-12 17:43:24 +0200

    Add styling to date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,15 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
     // const expenseDate = new Date(2021, 2, 28);
     // const expenseTitle = "Car Insurance";
     // const expenseAmount = 294.67;
-    const month = props.date.toLocaleString("en-US", { month: "long" });
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
 
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">$ {props.amount}</div>
