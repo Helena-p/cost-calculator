4bb30482c144d8c7f79e2cfc2776eeae6cebddbe
Helena <helenasleterkesto@gmail.com>
2021-05-14 22:30:59 +0200

    Organising files and folders

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,10 @@
 import "./Card.css";
 
-function Card(props) {
+const Card = (props) => {
     const classes = "card " + props.className;
     // tell the custom component to include the other classes as well. Note! the whitespace behind 'card '
     return <div className={classes}>{props.children}</div>;
     // dynamically point to the constants classes
-}
+};
 
 export default Card;
